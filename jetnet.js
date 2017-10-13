39ace7bb4c589f62b5f647214a6ea164615b476a
motoski <burgess.pedersen@gmail.com>
2017-10-13 16:54:21 -0700

    newlines

--- jetnet.js
+++ jetnet.js
@@ -1,6 +1,8 @@
+
 var net = require("net");
 var argv = process.argv.slice(2);
 var client = null;
+
 var init = function(print, error, quit, argv, keyboard) {
     client = new net.Socket();
     client.on("data", print);
@@ -12,17 +14,22 @@ var init = function(print, error, quit, argv, keyboard) {
     process.stdin.setEncoding("utf-8");
     process.stdin.on("data", keyboard);
 };
+
 var print = function(text) {
     process.stdout.write(text);
 };
+
 var keyboard = function(key) {
     client.write(key);
 };
+
 var error = function(e) {
     print(e.toString() + "\n");
     quit();
 };
+
 var quit = function() {
     process.exit();
 };
+
 init(print, error, quit, argv, keyboard);
