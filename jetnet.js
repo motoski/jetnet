ee38086a5f80cf728c4cdcb0d35c1a11e8c16fc1
motoski <burgess.pedersen@gmail.com>
2017-10-13 16:42:57 -0700

    minor runtime fixes

--- jetnet.js
+++ jetnet.js
@@ -1,34 +1,28 @@
 var net = require("net");
-
 var argv = process.argv.slice(2);
-
 var client = null;
-
-var init = function(argv, keyboard, print, connected, quit) {
+var init = function(print, error, quit, argv, keyboard) {
     client = new net.Socket();
-    client.connect(parseInt(argv[1], 10), argv[0], connected);
     client.on("data", print);
+    client.on("error", error);
     client.on("close", quit);
+    client.connect(parseInt(argv[1], 10), argv[0], function() { });
     process.stdin.resume();
     process.stdin.setRawMode(true);
     process.stdin.setEncoding("utf-8");
     process.stdin.on("data", keyboard);
 };
-
 var print = function(text) {
     process.stdout.write(text);
 };
-
 var keyboard = function(key) {
     client.write(key);
 };
-
-var connected = function() {
-    // -
+var error = function(e) {
+    print(e.toString() + "\n");
+    quit();
 };
-
 var quit = function() {
     process.exit();
 };
-
-init(argv, keyboard, print, connected, quit);
+init(print, error, quit, argv, keyboard);
